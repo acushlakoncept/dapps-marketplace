fd279ac5004d3a9e774f98f41b941be65ea951d0
Uduak Essien <acushla4real@gmail.com>
2022-01-12 16:03:39 +0100

    Update anchor tags with Link

--- components/common/navbar/index.js
+++ components/common/navbar/index.js
@@ -1,4 +1,4 @@
-
+import Link from 'next/link';
 
 
 export default function Navbar() {
@@ -8,13 +8,33 @@ export default function Navbar() {
               <nav className="relative" aria-label="Global">
                 <div className="flex justify-between">
                   <div>
-                    <a href="#" className="font-medium mr-8 text-gray-500 hover:text-gray-900">Product</a>
-                    <a href="#" className="font-medium mr-8 text-gray-500 hover:text-gray-900">Features</a>
-                    <a href="#" className="font-medium mr-8 text-gray-500 hover:text-gray-900">Marketplace</a>
+                    <Link href="/">
+                      <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">
+                        Home
+                      </a>
+                    </Link>
+                    <Link href="/">
+                      <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">
+                        Blogs
+                      </a>
+                    </Link>
+                    <Link href="/">
+                      <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">
+                      Marketplace
+                      </a>
+                    </Link>
                   </div>
                   <div>
-                    <a href="#" className="font-medium mr-8 text-gray-500 hover:text-gray-900">Company</a>
-                    <a href="#" className="font-medium mr-8 text-indigo-600 hover:text-indigo-500">Log in</a>
+                  <Link href="/">
+                    <a  className="font-medium mr-8 text-gray-500 hover:text-gray-900">
+                      Wishlist
+                    </a>
+                  </Link>
+                  <Link href="/">
+                      <a className="font-medium mr-8 text-indigo-600 hover:text-indigo-500">
+                        Log in
+                      </a>
+                  </Link>
                   </div>
                 </div>
               </nav>
