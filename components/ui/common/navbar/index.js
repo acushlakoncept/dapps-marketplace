354e19ed3d26240015d81ba85cab6d4631ff4c2b
Uduak Essien <acushla4real@gmail.com>
2022-01-13 10:31:30 +0100

    Implement connect metamask button

--- components/ui/common/navbar/index.js
+++ components/ui/common/navbar/index.js
@@ -1,14 +1,18 @@
 import { useWeb3 } from '@components/providers';
 import Link from 'next/link';
+import { Button } from '@components/ui/common';
+import { useRouter } from 'next/router';
 
 
 export default function Navbar() {
-  const { connect } = useWeb3();
+  const { connect, isLoading, isWeb3Loaded } = useWeb3();
+  const router = useRouter();
+
     return (
         <section>
             <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
               <nav className="relative" aria-label="Global">
-                <div className="flex justify-between">
+                <div className="flex justify-between items-center">
                   <div>
                     <Link href="/">
                       <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">
@@ -32,11 +36,18 @@ export default function Navbar() {
                       Wishlist
                     </a>
                   </Link>
-                    <span  
-                      onClick={connect}
-                      className="px-8 py-3 border rounded-md text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
-                        Connect
-                    </span>
+                    {
+                      isLoading ?
+                      <Button 
+                        disabled={true}
+                        onClick={connect} >Loading...</Button> : 
+                      isWeb3Loaded ? 
+                      <Button onClick={connect} >Connect</Button> :
+                      <Button 
+                        onClick={() => router.push("https://metamask.io/download.html")}>
+                          Install Metamask
+                      </Button>
+                    }
                   </div>
                 </div>
               </nav>
