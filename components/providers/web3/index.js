04c82fb60f9d2cb599a944676991110388fe7e6e
Uduak Essien <acushla4real@gmail.com>
2022-01-15 17:32:28 +0100

    Add useHooks function

--- components/providers/web3/index.js
+++ components/providers/web3/index.js
@@ -3,6 +3,7 @@ const { Children } = require("react/cjs/react.production.min");
 
 import detectEthereumProvider from "@metamask/detect-provider";
 import Web3 from "web3";
+import { setupHooks } from "./hooks/setupHooks";
 
 const Web3Context = createContext(null);
 
@@ -36,13 +37,15 @@ export default function Web3Provider({children}) {
     }, []);
 
     const _web3Api = useMemo(() => {
+        const { web3, provider} = web3Api;
         return {
             ...web3Api,
             isWeb3Loaded: web3Api.web3 != null,
-            connect: web3Api.provider ?
+            getHooks: () => setupHooks(web3),
+            connect: provider ?
                 async () => {
                     try {
-                        await web3Api.provider.request({ method: "eth_requestAccounts" });
+                        await provider.request({ method: "eth_requestAccounts" });
                     } catch  {
                         location.reload();
                     }
@@ -60,4 +63,9 @@ export default function Web3Provider({children}) {
 
 export function useWeb3() {
     return useContext(Web3Context);
+}
+
+export function useHooks(cb) {
+    const { getHooks } = useWeb3();
+    return cb(getHooks());
 }
