e77a971974247579e04f81f17a249cd4b81c6f45
Uduak Essien <acushla4real@gmail.com>
2022-01-18 15:49:36 +0100

    Update market place with eth rates

--- pages/marketplace/index.js
+++ pages/marketplace/index.js
@@ -1,12 +1,18 @@
+import { useEthPrice } from "@components/hooks/useEthPrice";
 import { useAccount, useNetwork } from "@components/hooks/web3";
+import { Button } from "@components/ui/common";
 import { CourseCard, CourseList } from "@components/ui/course";
 import { BaseLayout } from "@components/ui/layout";
-import { WalletBar } from "@components/ui/web3";
+import { OrderModal } from "@components/ui/order";
+import { EthRates, WalletBar } from "@components/ui/web3";
 import { getAllCourses } from "@content/courses/fetcher";
+import { useState } from "react";
 
 export default function Marketplace({courses}) {
+  const [selectedCourse, setSelectedCourse] = useState(null);
     const {account} = useAccount();
     const { network } = useNetwork();
+    const { eth } = useEthPrice();
 
   return (
       <>
@@ -20,15 +26,31 @@ export default function Marketplace({courses}) {
               hasInitialResponse: network.hasInitialResponse,
             }}
              /> 
-
+            <EthRates eth={eth.data} ethPerItem={eth.perItem} />
         </div>
         <CourseList courses={courses}>
           {course => 
             <CourseCard 
               key={course.id} 
-              course={course} 
+              course={course}
+              Footer={() => 
+              <div className="mt-4">
+                <Button 
+                variant="lightPurple"
+                onClick={() => setSelectedCourse(course)}
+                >
+                  Purchase
+                </Button>
+              </div>
+                }
             />}
         </CourseList>
+        { selectedCourse &&
+          <OrderModal 
+            course={selectedCourse}
+            onClose={() => setSelectedCourse(null)}
+           />
+        }
       </>
   )
 }
