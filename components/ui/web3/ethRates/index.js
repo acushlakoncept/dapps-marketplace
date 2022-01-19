32ff679eb0e175576d4c64e55b345e1d30cecedb
Uduak Essien <acushla4real@gmail.com>
2022-01-19 11:13:24 +0100

    Refactor marketplace, add header, move props to unit component

--- components/ui/web3/ethRates/index.js
+++ components/ui/web3/ethRates/index.js
@@ -1,8 +1,11 @@
+import { useEthPrice } from "@components/hooks/useEthPrice";
 import Image from "next/image";
 
 
 
-export default function EthRates({eth, ethPerItem}) {
+export default function EthRates() {
+  const { eth } = useEthPrice();
+
     return (
       <div className="grid grid-cols-4">
         <div className="flex flex-1 items-stretch text-center">
@@ -15,7 +18,7 @@ export default function EthRates({eth, ethPerItem}) {
                 src="/small-eth.webp"
                 alt="small-eth"
                 />
-              <span className="text-2xl font-bold"> = {eth}$</span>
+              <span className="text-2xl font-bold"> = {eth.data}$</span>
             </div>
             <p className="text-xl text-gray-500">Current eth Price</p>
           </div>
@@ -24,7 +27,7 @@ export default function EthRates({eth, ethPerItem}) {
           <div className="p-10 border drop-shadow rounded-md">
             <div className="flex items-center">
               <span className="text-2xl font-bold">
-                {ethPerItem}
+                {eth.perItem}
               </span>
               <Image
                 layout="fixed"
