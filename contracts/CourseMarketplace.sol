7fefcb0ba19235a8eb9eb69f5acab7c3afc598c2
Uduak Essien <acushla4real@gmail.com>
2022-01-20 14:44:06 +0100

    Add courseMarketplace contract, add coursehash and store course function

--- contracts/CourseMarketplace.sol
+++ contracts/CourseMarketplace.sol
@@ -18,4 +18,29 @@ contract CourseMarketplace {
         State state; //1 byte
     }
 
+    // mapping of courseHash to Course data
+    mapping(bytes32 => Course) private ownedCourses;
+
+    // mapping of courseId to courseHash
+    mapping(uint => bytes32) private ownedCourseHash;
+
+    // number of all courses + id of the course
+    uint private totalOwnedCourses;
+
+    function purchaseCourse(bytes16 courseId, bytes32 proof) 
+    external payable
+    {
+        bytes32 courseHash = keccak256(abi.encodePacked(courseId, msg.sender));
+        uint id = totalOwnedCourses++;
+        
+        ownedCourseHash[id] = courseHash;
+        ownedCourses[courseHash] = Course({
+            id: id,
+            price: msg.value,
+            proof: proof,
+            owner: msg.sender,
+            state: State.Purchased
+        });
+    }
+
 }
