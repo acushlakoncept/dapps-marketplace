3efa12c8f2f32cb065339dcb858ba273f5643d98
Uduak Essien <acushla4real@gmail.com>
2022-01-20 10:44:25 +0100

    Add course filter component

--- components/hooks/useEthPrice.js
+++ components/hooks/useEthPrice.js
@@ -5,7 +5,7 @@ import useSWR from "swr"
 
 const URL = "https://api.coingecko.com/api/v3/coins/ethereum?localization=false&tickers=false&community_data=false&developer_data=false&sparkline=false";
 const ETH_USD = "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd";
-const COURSE_PRICE = 15;
+export const COURSE_PRICE = 15;
 
 const fetcher = async url => {
     const res = await fetch(url)
