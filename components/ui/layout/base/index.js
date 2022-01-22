5e39f0baf4ea7a5734a5b91b29f7c53ade4f0ed6
Uduak Essien <acushla4real@gmail.com>
2022-01-22 14:33:17 +0100

    Load contract using web3

--- components/ui/layout/base/index.js
+++ components/ui/layout/base/index.js
@@ -2,9 +2,9 @@ import { Web3Provider } from "@components/providers";
 import { Footer, Navbar } from "@components/ui/common";
 
 
-
 export default function BaseLayout({children}) {
     return (
+
         <Web3Provider>
         <div className="relative max-w-7xl mx-auto px-4">
            <Navbar />
--- utils/loadContracts.js
+++ utils/loadContracts.js
@@ -1,18 +1,16 @@
-import contract from "@truffle/contract";
 
-export const loadContract = async (name, provider) => {
+export const loadContract = async (name, web3) => {
   const res = await fetch(`/contracts/${name}.json`)
   const Artifact = await res.json()
+  let contract = null;
 
-  const _contract = contract(Artifact)
-  _contract.setProvider(provider)
-
-  let deployedContract = null
   try {
-    deployedContract = await _contract.deployed();
+    contract = new web3.eth.Contract(
+      Artifact.abi, 
+      Artifact.networks[process.env.NEXT_PUBLIC_NETWORK_ID].address)
   } catch {
     console.log(`Contract ${name} cannot be loaded - Connect to the right network`)
   }
 
-  return deployedContract
+  return contract;
 }
