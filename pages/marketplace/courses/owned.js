1f6819f9a70c8d49da3fc9752220251ca69a4414
Uduak Essien <acushla4real@gmail.com>
2022-01-24 13:32:37 +0100

    Update owned courses page to display user's courses purchased

--- pages/marketplace/courses/owned.js
+++ pages/marketplace/courses/owned.js
@@ -1,34 +1,48 @@
-import { useOwnedCourses } from "@components/hooks/web3";
+import { useOwnedCourses, useWalletInfo } from "@components/hooks/web3";
 import { Button, Message } from "@components/ui/common";
 import { OwnedCourseCard } from "@components/ui/course";
 import { BaseLayout } from "@components/ui/layout";
 import { MarketHeader } from "@components/ui/marketplace";
+import { getAllCourses } from "@content/courses/fetcher";
 
 
 
-export default function OwnedCourses() {
-
-    const { ownedCourses} = useOwnedCourses();
+export default function OwnedCourses({courses}) {
+    const { account } = useWalletInfo();
+    const { ownedCourses } = useOwnedCourses(courses, account.data);
 
 
     return (
         <>
-          {ownedCourses.data}
           <div className="py-4">
             <MarketHeader />
           </div>
           <section className="grid grid-cols-1">
-          <OwnedCourseCard>
-          <Message>
-            My custom message
-          </Message>
-          <Button>
-            Watch the course
-          </Button>
-        </OwnedCourseCard>
+            { ownedCourses.data?.map(course => 
+            <OwnedCourseCard
+              key={course.id}
+              course={course}
+            >
+              {/* <Message>
+                My custom message
+              </Message> */}
+              <Button>
+                Watch the course
+              </Button>
+            </OwnedCourseCard>
+            )}
           </section>
         </>
     )
+}
+
+export function getStaticProps() {
+  const {data} = getAllCourses();
+  return {
+    props: {
+      courses: data
+    }
+  }
 }
 
 OwnedCourses.Layout = BaseLayout
