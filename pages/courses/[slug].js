695d14b6881cc504188bd5245cfb2274fb2936b7
Uduak Essien <acushla4real@gmail.com>
2022-01-26 11:13:28 +0100

    Update message component on course detail page to show course status to owner

--- pages/courses/[slug].js
+++ pages/courses/[slug].js
@@ -1,4 +1,5 @@
-import { Modal } from "@components/ui/common";
+import { useAccount, useOwnedCourse } from "@components/hooks/web3";
+import { Message, Modal } from "@components/ui/common";
 import {
   CourseHero,
   Curriculum,
@@ -9,16 +10,42 @@ import { getAllCourses } from "@content/courses/fetcher";
 
 export default function Course({course}) {
 
+  const { account } = useAccount();
+  const { ownedCourse } = useOwnedCourse(course, account.data);
+  const courseState = ownedCourse.data?.state;
+
   return (
     <>
       <div className="py-4">
-        <CourseHero title={course.title}
-                    description={course.description}
-                    image={course.coverImage} />
+        <CourseHero 
+          hasOwner={!!ownedCourse.data}
+          title={course.title}
+          description={course.description}
+          image={course.coverImage} />
       </div>
       <Keypoints
         points={course.wsl}
        />
+       <div className="max-w-5xl mx-auto">
+       { courseState === "purchased" && 
+          <Message type="warning">
+            Course is purchased and waiting for activation. Process can take up to 24 hours.
+            <i className="block font-normal">Incase of any questions, please contact info@acushlakoncept.com</i>
+          </Message>
+       }
+       { courseState === "activated" && 
+          <Message>
+            Acushla Innovations wishes you a happy learning experience.
+          </Message>
+       }
+       { courseState === "deactivated" && 
+          <Message type="danger">
+            Course has been deactivated, due to an incorrect purchase data.
+            The functionality to watch the course has been temporaly disabled.
+            <i className="block font-normal">Incase of any questions, please contact info@acushlakoncept.com</i>
+          </Message>
+       }
+       </div>
       <Curriculum locked={true} />
       <Modal />
     </>
