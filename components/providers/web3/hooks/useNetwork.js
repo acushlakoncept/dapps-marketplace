4e1e4adcd46914cacba98271b48d4c9f2ff88adb
Uduak Essien <acushla4real@gmail.com>
2022-01-26 15:43:02 +0100

    Refactore useAccount and useNetwork hooks to minimize rate of useEffect calls

--- components/providers/web3/hooks/useNetwork.js
+++ components/providers/web3/hooks/useNetwork.js
@@ -23,16 +23,24 @@ export const handler = (web3, provider) => () => {
         web3 ? "web3/network" : null,
         async () => {
           const chainId = await web3.eth.getChainId();
-          return NETWORKS[chainId] ?? chainId;
+
+          if(!chainId) {
+              throw new Error("Cannot retrieve a network, Please refresh the browser");
+          }
+
+          return NETWORKS[chainId];
         }
     )
 
     useEffect(() => {
-        provider &&
-        provider.on("chainChanged", chainId => {
-            mutate(NETWORKS[parseInt(chainId, 16)])
-        })
-    }, [web3]);
+        const mutator = chainId => mutate(NETWORKS[parseInt(chainId, 16)]);
+        provider?.on("chainChanged", mutator)
+  
+        return () => {
+           provider?.removeListener("chainChanged", mutator)
+        }
+ 
+     }, [provider])
     
     return {
         data,
